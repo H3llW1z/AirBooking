3ae6c39726991774fddef72eabc5ce0ee1f074d2
panassevich <panassevichm@gmail.com>
2023-10-04 18:01:56 +0300

    added basic project structure

--- app/src/main/java/pt/course/airbooking/ui/MainActivity.kt
+++ app/src/main/java/pt/course/airbooking/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package pt.course.airbooking
+package pt.course.airbooking.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import pt.course.airbooking.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
