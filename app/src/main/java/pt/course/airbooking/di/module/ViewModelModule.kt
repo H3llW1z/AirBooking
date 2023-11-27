280d107f6da3e24a16af43b8843e07b749d42817
Ilya Balakirev <IlyaBalakirev2003@gmail.com>
2023-11-27 18:43:46 +0300

    Add Dependency injection in ViewModel

--- app/src/main/java/pt/course/airbooking/di/module/ViewModelModule.kt
+++ app/src/main/java/pt/course/airbooking/di/module/ViewModelModule.kt
@@ -5,8 +5,13 @@ import dagger.Binds
 import dagger.Module
 import dagger.multibindings.IntoMap
 import pt.course.airbooking.di.annotation.ViewModelKey
+import pt.course.airbooking.presentation.congratulations.CongratulationsViewModel
+import pt.course.airbooking.presentation.flightinformation.FlightInformationViewModel
+import pt.course.airbooking.presentation.flightinformationchange.FlightInformationChangeViewModel
 import pt.course.airbooking.presentation.flightslist.FlightsListViewModel
 import pt.course.airbooking.presentation.initial.InitialViewModel
+import pt.course.airbooking.presentation.personaldocument.PersonalDocumentViewModel
+import pt.course.airbooking.presentation.planeslist.PlanesListViewModel
 
 @Module
 interface ViewModelModule {
@@ -20,4 +25,29 @@ interface ViewModelModule {
     @IntoMap
     @ViewModelKey(FlightsListViewModel::class)
     fun bindFlightsListViewModel(viewModel: FlightsListViewModel): ViewModel
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(PlanesListViewModel::class)
+    fun bindPlanesListViewModel(viewModel: PlanesListViewModel): ViewModel
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(CongratulationsViewModel::class)
+    fun bindCongratulationsViewModel(viewModel: CongratulationsViewModel): ViewModel
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(FlightInformationViewModel::class)
+    fun bindFlightInformationViewModel(viewModel: FlightInformationViewModel): ViewModel
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(FlightInformationChangeViewModel::class)
+    fun bindFlightInformationChangeViewModel(viewModel: FlightInformationChangeViewModel): ViewModel
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(PersonalDocumentViewModel::class)
+    fun bindPersonalDocumentViewModel(viewModel: PersonalDocumentViewModel): ViewModel
 }
